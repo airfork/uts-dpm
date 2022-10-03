19f0e324e5597688076f108175e3dd401d102bb7
Tunji Afolabi-Brown <airfork@gmail.com>
2022-10-03 09:52:15 -0400

    only logging requests in local profile

--- src/main/kotlin/com/tunjicus/utsdpm/configs/AppConfig.kt
+++ src/main/kotlin/com/tunjicus/utsdpm/configs/AppConfig.kt
@@ -1,16 +1,21 @@
 package com.tunjicus.utsdpm.configs
 
 import com.tunjicus.utsdpm.interceptors.RequestLoggingInterceptor
+import org.springframework.core.env.Environment
 import org.springframework.stereotype.Component
 import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Component
-class AppConfig(private val requestLoggingInterceptor: RequestLoggingInterceptor) :
-  WebMvcConfigurer {
+class AppConfig(
+  private val requestLoggingInterceptor: RequestLoggingInterceptor,
+  private val environment: Environment
+) : WebMvcConfigurer {
   override fun addInterceptors(registry: InterceptorRegistry) {
-    registry.addInterceptor(requestLoggingInterceptor)
+    if (environment.activeProfiles.contains("local")) {
+      registry.addInterceptor(requestLoggingInterceptor)
+    }
   }
 
   override fun addCorsMappings(registry: CorsRegistry) {
