e573015f637b65a78025a6c0de004fe13f5f90e5
Tunji Afolabi-Brown <airfork@gmail.com>
2026-01-11 20:58:40 -0500

    fixing issue with approvals page as manager

--- db_scripts/scripts/insert_users.sql
+++ db_scripts/scripts/insert_users.sql
@@ -11,6 +11,11 @@ INSERT INTO users (managerid, username, password, firstname, lastname, fulltime,
 VALUES (1, 'john@doe.com', '$2a$10$vg3C7Eir1IgaYYAOoZQw6.N6ilmYU6DcghHuym3NYWA2mzVT9ryqK', 'John',
         'Doe', true, true, 0, now());
 
+-- Admin
 INSERT INTO user_roles (user_id, role_id) VALUES (1, 1);
+
+-- Manager
 INSERT INTO user_roles (user_id, role_id) VALUES (2, 2);
+
+-- Analyst
 INSERT INTO user_roles (user_id, role_id) VALUES (3, 3);
